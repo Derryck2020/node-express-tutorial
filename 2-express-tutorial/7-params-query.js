1dfeaf93c9376eccb72ef10760263f94228e4e9f
Derryck <oseiagyemanb@gmail.com>
2022-11-15 21:07:43 +0100

    initial commit

--- 2-express-tutorial/7-params-query.js
+++ 2-express-tutorial/7-params-query.js
@@ -59,9 +59,12 @@ app.get('/api/v1/query', (req,res) =>{
     if(limit){
         sortedProducts = sortedProducts.slice(0, Number(limit))
     }
+    if(sortedProducts.length < 1){
+        //res.status(200).send('no products match your search') 
+        return res.status(200).json({sucess: true, data: []})
+    }
 
     res.status(200).json(sortedProducts)
-        //res.send('Hello world')
 })
 
 app.listen(5000, () =>{
